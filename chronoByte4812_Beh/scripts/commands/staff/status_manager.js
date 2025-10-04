f43078b9cac26fb27a9a6f795bd5893392ee74e6
chronoByte4812 <129338316+chronoByte4812@users.noreply.github.com>
2025-10-04 11:03:06 +0930

    Update status_manager.js

--- chronoByte4812_Beh/scripts/commands/staff/status_manager.js
+++ chronoByte4812_Beh/scripts/commands/staff/status_manager.js
@@ -1,1 +1,33 @@
+import { commandBuild } from '../core/classes/commandBuilder.js'
+import { config } from "../core/config.js";
 
+commandBuild.register(
+    {
+        name: 'status',
+        description: 'Manage status tags on a target or self',
+        for_staff: true,
+        usage: [
+            'status',
+            'status list @player'
+          //...
+        ],
+        aliases: [],
+        examples: [
+            'status add @player <tag id'
+            'status list @player'
+        ]
+    },
+    (data, args) => {
+        const sender = data.sender;
+        const isStaff = sender.hasTag(config.staffTag);
+        let text = [];
+
+        if (args.length < 1) {
+        
+        };
+   }; 
+
+        sender.sendMessage(text.join('§r\n'))
+    },
+    (data, args) => { }
+);
